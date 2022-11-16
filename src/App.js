234396946377101c736de678717ed013bfc8c537
Prajwali Shetty <prajwali.shetty15@gmail.com>
2022-11-16 22:50:33 +0530

    class 01

---
+++ src/App.js
@@ -0,0 +1,6 @@
+//const heading = React.createElement('h1',{},'Hello world from React');
+
+// adding id to h1 element
+const heading = React.createElement('h1',{id:'title'},'Hello world from React');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
